bd1bfea0342069897dcd0b48b0620546774098b5
rakib06 <six.rakib@gmail.com>
2020-01-25 12:28:24 +0600

    first commit

---
+++ mysite_root/polls/views_old_2.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import Http404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+
+from .models import Question, Choice
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html',context)
+
+
+# short cut
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'ques': question})
+
+def detail2(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Sorry, Question does not exist")
+    return render(request, 'polls/detail.html', {'ques': question})
+
+
+def vote(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting from
+        return render(request, 'polls/detail.html',{
+            'ques':question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        '''
+        Always return an HttpResponseRedirect after successfully
+        dealing with POST data. This prevents data from being posted twice.
+        if a user hits the back button.
+        '''
+        #
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question': question})
+
+
